23edf3b23d8937b49a404b9ca6cbd9120464e34d
doneg1 <122523554+doneg1@users.noreply.github.com>
2023-01-17 17:40:25 +0200

    Add files via upload

--- XMLtest.py
+++ XMLtest.py
@@ -3,17 +3,53 @@ import xml.etree.ElementTree as ET
 def check_xml(folder):
     pass
 
+
 tree = ET.parse("TextCheck.xml")
 root = tree.getroot()
-root1 = root
-# print((root[0]).tag)
-for item in root1:
-    if len(root1) > 0:
-        root1 = root1[0]
-        print(root1.tag)
+sf = 0
+
+myArray = []
+print(len(root))
+for elem in root:
+    print("folder: " + elem.get("name"))
+    folder_id = elem.get("name")
+    myArray.append({'folder': folder_id})
+    for item in root[sf]:
+        print(item.get("name"))
+    print("--------")
+    sf += 1
+
+    # invoice_id = elem.get('Invoice_ID')
+    # invoice_owner_id = elem.get('Invoice_Owner_ID')
+    # icn_id = elem.get('ICN_ID')
+    # icn_owner_id = elem.get('ICN_Owner_ID')
+    # receipt_date = elem.get('Receipt_Date')
+    # myArray.append({'Invoice_ID': invoice_id, 'Invoice_Owner_ID': invoice_owner_id, 'ICN_ID': icn_id, 'ICN_Owner_ID': icn_owner_id, 'Receipt_Date': receipt_date})
+print("----------------------------------------------------------------------")
+for x in myArray:
+    print(x)
 
 
-print(root.tag)
+
+
+
+
+
+
+
+
+# tree = ET.parse("TextCheck.xml")
+# root = tree.getroot()
+# root1 = root
+# flag=1
+# # print((root[0]).tag)
+# for item in root1:
+#     if len(root1) > 0:
+#         root1 = root1[0]
+#     flag *= len(item)
+#     print(flag)
+
+
     # for elem in root[0]:
     #     if elem.tag == "folder":
     #         print("ok")
